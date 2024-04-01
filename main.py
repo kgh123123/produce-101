b6cc97c0e40cfd0d9a00564939a62f110dac7df6
rrayy <rrayy25809@gmail.com>
2024-04-01 17:50:41 +0900

    venv 가상환경 사용

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def main():
     return render_template('index.html',name="유저")
 
+@app.route('/login')
+def login():
+    return render_template('index.html')
+
+@app.route('/dashborad')
+def dashborad():
+    return render_template('index.html',name="유저")
+
 if __name__ == '__main__':
     app.run(debug=True)
