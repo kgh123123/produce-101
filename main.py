808c9a8309447f04cadfba0c44a3b37ffe21ed76
rrayy <rrayy25809@gmail.com>
2024-04-06 21:19:53 +0900

    venv 파일 삭제

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,jsonify
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,13 +7,37 @@ app = Flask(__name__)
 def main():
     return render_template('index.html',name="유저")
 
-@app.route('/login')
-def login():
-    return render_template('index.html')
+# SQLite 데이터베이스 초기화
+def init_db():
+    conn = sqlite3.connect('user.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            number TEXT NOT NULL,
+            name TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
-@app.route('/dashborad')
-def dashborad():
-    return render_template('index.html',name="유저")
+# 클라이언트로부터 전송된 정보를 데이터베이스에 저장하는 함수
+def save_to_database(number,name):
+    conn = sqlite3.connect('user.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO users (number,name) VALUES (?, ?)', (number,name))
+    conn.commit()
+    conn.close()
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    number = data.get('number')
+    name = data.get('name')
+    save_to_database(number,name)
+    # 데이터베이스에 저장
+    return jsonify({'message': 'Data received and saved successfully!'})
 
 if __name__ == '__main__':
+    init_db()  # 데이터베이스 초기화
     app.run(debug=True)
