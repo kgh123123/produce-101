6e4f9b00f245a58dafaad5c2aca3c3800f3267c4
rrayy <rrayy25809@gmail.com>
2024-04-21 13:37:38 +0900

    Changes to be committed:중간 작업물 커밋, flask-WTF 라이브러리 추가 사용 	modified:   main.py 	modified:   requirements.txt

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from flask import Flask, render_template,request,jsonify
+from flask import Flask, session,render_template, request, redirect, url_for, flash
 import sqlite3
 
 app = Flask(__name__)
+conn = sqlite3.connect('user.db')
+cursor = conn.cursor()
 
-@app.route('/')
-def main():
-    return render_template('index.html',name="유저")
-
-# SQLite 데이터베이스 초기화
+#SQLite 데이터베이스 초기화
 def init_db():
-    conn = sqlite3.connect('user.db')
-    cursor = conn.cursor()
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,22 +17,36 @@ def init_db():
     conn.commit()
     conn.close()
 
-# 클라이언트로부터 전송된 정보를 데이터베이스에 저장하는 함수
-def save_to_database(number,name):
-    conn = sqlite3.connect('user.db')
-    cursor = conn.cursor()
-    cursor.execute('INSERT INTO users (number,name) VALUES (?, ?)', (number,name))
-    conn.commit()
-    conn.close()
+@app.route('/')
+def index():
+    userid = session.get('userid', None)
+    return render_template("index.html",name=userid)
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET','POST'])  
 def login():
-    data = request.get_json()
-    number = data.get('number')
-    name = data.get('name')
-    save_to_database(number,name)
-    # 데이터베이스에 저장
-    return jsonify({'message': 'Data received and saved successfully!'})
+    session['userid'] = 'userid' #form에서 가져온 userid를 session에 저장
+    return redirect('/') #로그인에 성공하면 홈화면으로 redirect     
+    #return render_template('login.html', form=form)
+
+
+#TODO:GPT의 힘을 빌린 투표코드(현재 따옴표로 주석처리), 로그인 기능 완성 후 연동
+'''@app.route('/')
+def index():
+    # 모든 후보자 가져오기
+    cursor.execute("SELECT * FROM votes")
+    candidates = cursor.fetchall()
+    return render_template('index.html', candidates=candidates)
+
+@app.route('/vote', methods=['POST'])
+def vote():
+    candidate = request.form['candidate']
+    
+    # 후보자의 득표수 업데이트
+    cursor.execute("UPDATE votes SET votes = votes + 1 WHERE candidate = ?", (candidate,))
+    conn.commit()
+    
+    flash('투표가 성공적으로 기록되었습니다!', 'success')
+    return redirect(url_for('index'))'''
 
 if __name__ == '__main__':
     init_db()  # 데이터베이스 초기화
