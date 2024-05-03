3e7e1f44563dae0df32faf133eea46dd0c8046be
rrayy <rrayy25809@gmail.com>
2024-05-03 12:56:47 +0900

    Changes to be committed:네비게이션 바 수정, 로그인 에러 수정     modified:   main.py 	modified:   static/navigation.html 	modified:   templates/login.html 	new file:   templates/vote.html

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ from flask import Flask, session, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
-conn = sqlite3.connect('user.db')
+conn = sqlite3.connect('user.db', isolation_level= None)
 cursor = conn.cursor()
 app.secret_key = 'LN$oaYB9-5KBT7G'
+
+login_error:bool = False
 
 #SQLite 데이터베이스 초기화
 def init_db():
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            number TEXT NOT NULL,
-            name TEXT NOT NULL
+            number INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            count INTEGER NOT NULL,
+            manager BOOLEAN NOT NULL
         )
     ''')
     conn.commit()
@@ -27,18 +31,31 @@ def index():
 @app.route('/login')
 def login():
     name:str = session.get('name', "로그인되지 않음")
-    return render_template("login.html",name=name)
+    already_login:str = 'disabled' if name == "로그인되지 않음" else ' '
+    return render_template("login.html",name=name, is_login_error=str(int(login_error)), is_already_login=already_login)
+
+@app.route('/vote')
+def vote():
+    name:str = session.get('name', "로그인되지 않음")
+    return render_template("vote.html",name=name)
 
 @app.route('/dashboard')
 def dashboard():
     name:str = session.get('name', "로그인되지 않음")
     return render_template("dashboard.html",name=name)
 
-@app.route('/POST_login', methods=['POST'])  
-def POST_login  ():
-    name = request.form['name']  # 폼 데이터에서 'userid' 값을 가져옴
-    session['name'] = name #form에서 가져온 userid를 session에 저장
-    return redirect('/') #로그인에 성공하면 홈화면으로 redirect     
+@app.route('/POST_login', methods=['POST'])
+def POST_login():
+    global login_error
+    try:
+        name = request.form['name']  # 폼 데이터에서 'userid' 값을 가져옴
+        cursor.execute("")
+        session['name'] = name #form에서 가져온 userid를 session에 저장
+        login_error = False
+        return redirect('/') #로그인에 성공하면 홈화면으로 redirect
+    except:
+        login_error = True
+        return redirect('/login') #로그인에 실패하면 로그인화면으로 redirect
 
 
 #TODO:GPT의 힘을 빌린 투표코드(현재 따옴표로 주석처리), 로그인 기능 완성 후 연동
@@ -61,5 +78,5 @@ def vote():
     return redirect(url_for('index'))'''
 
 if __name__ == '__main__':
-    init_db()  # 데이터베이스 초기화
+    #init_db()  #데이터베이스를 초기화시키는 명령이니 되도록 주석을 풀지 말 것
     app.run(debug=True)
