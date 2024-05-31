46491be3106387208babc6b5ecba736c37577fc2
rrayy <rrayy25809@gmail.com>
2024-05-31 14:38:44 +0900

    Changes to be committed:데이터베이스 수정, 투표 기능 일부 수정 	modified:   main.py 	modified:   readme.md 	modified:   templates/vote.html 	modified:   user.db

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ conn = sqlite3.connect('user.db', check_same_thread=False)
 cursor = conn.cursor()
 app.secret_key = 'LN$oaYB9-5KBT7G'
 
+#SQLite 데이터베이스 초기화
+def init_db():
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            "number"	INTEGER,
+            "name"	TEXT,
+            "count"	INTEGER,
+            "is_manager"	INTEGER,
+            "is_already_vote"	INTEGER,
+            "reason"	TEXT
+        );
+    ''')
+    conn.commit()
+    conn.close()
+
 @app.route('/')
 def index():
     name = session.get('name', "로그인되지 않음")
@@ -19,16 +34,21 @@ def login():
 @app.route('/vote')
 def vote():
     name = session.get('name', "로그인되지 않음")
-    return render_template("vote.html", name=name,candidates=["하나","둘"])
+    cursor.execute('SELECT number, name FROM users')
+    candidates=[str(row[0])+" "+row[1] for row in cursor.fetchall()]
+    return render_template("vote.html", name=name,candidates=candidates)
+
+@app.route('/POST_vote', methods=['POST'])
+def POST_vote():
+    number = request.form['number']
 
 @app.route('/dashboard')
 def dashboard():
     name = session.get('name', "로그인되지 않음")
     return render_template("dashboard.html", name=name)
 
-@app.route('/POST_login', methods=['POST'])  
+@app.route('/POST_login', methods=['POST'])
 def POST_login():
-    #try:
     number = request.form['number']
     name = request.form['name']
     cursor.execute('SELECT number, name FROM users')
@@ -37,7 +57,6 @@ def POST_login():
     names = [row[1] for row in rows]
     if name in names and int(number) in numbers:
         session['name'] = name
-        session.pop('login_error', None)
         return redirect('/')
     else:
         raise Exception("동아리 인원 외의 유저가 로그인을 시도하였습니다.")
@@ -48,4 +67,5 @@ def logout():
     return redirect('/')
 
 if __name__ == '__main__':
+    #init_db()
     app.run(debug=True)
